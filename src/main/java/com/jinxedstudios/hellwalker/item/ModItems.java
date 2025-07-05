cabd60c5aa451b7a0d1add5ca555fb043586e0e3
CharlottePlaysGames <sharkattackjack@outlook.com>
2025-07-05 15:00:03 -0700

    ModItems.java added

---
+++ src/main/java/com/jinxedstudios/hellwalker/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.jinxedstudios.hellwalker.item;
+
+import com.jinxedstudios.hellwalker.HellwalkerEngine;
+
+public class ModItems {
+
+    public static void registerModItems(){
+        HellwalkerEngine.LOGGER.info("Registering Mod Items for " + HellwalkerEngine.MOD_ID);
+    }
+}
