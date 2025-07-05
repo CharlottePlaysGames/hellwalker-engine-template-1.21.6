ea73cda2a9ea9225c664bba774558b59559b5197
CharlottePlaysGames <sharkattackjack@outlook.com>
2025-07-05 13:58:19 -0700

    initial_commit

---
+++ src/main/java/com/jinxedstudios/hellwalker/HellwalkerClient.java
@@ -0,0 +1,10 @@
+package com.jinxedstudios.hellwalker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HellwalkerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/jinxedstudios/hellwalker/HellwalkerEngineDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jinxedstudios.hellwalker;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HellwalkerEngineDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
