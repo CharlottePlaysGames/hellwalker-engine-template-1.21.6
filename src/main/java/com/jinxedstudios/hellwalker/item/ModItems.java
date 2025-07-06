9f32e93e2767492d5206b66c17e63d9c5a1abcfc
CharlottePlaysGames <sharkattackjack@outlook.com>
2025-07-05 17:27:02 -0700

    Blue Keycard and Yellow Keycard added

--- src/main/java/com/jinxedstudios/hellwalker/item/ModItems.java
+++ src/main/java/com/jinxedstudios/hellwalker/item/ModItems.java
@@ -10,17 +10,18 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item BLUE_KEYCARD = registerItem("blue_keycard", new Item(new Item.Settings()));
-
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(HellwalkerEngine.MOD_ID, name), item);
     }
 
-    public static void registerModItems(){
+    public static void registerModItems() {
         HellwalkerEngine.LOGGER.info("Registering Mod Items for " + HellwalkerEngine.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
-            fabricItemGroupEntries.add(BLUE_KEYCARD);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(BLUE_KEYCARD);
+            entries.add(RAW_PINK_GARNET);
         });
     }
 }
