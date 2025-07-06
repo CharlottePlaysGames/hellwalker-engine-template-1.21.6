6a064427aafbf16a3537f882f2ff10f33c6e3768
CharlottePlaysGames <sharkattackjack@outlook.com>
2025-07-05 17:02:39 -0700

    Blue Keycard added

--- src/main/java/com/jinxedstudios/hellwalker/item/ModItems.java
+++ src/main/java/com/jinxedstudios/hellwalker/item/ModItems.java
@@ -1,10 +1,26 @@
 package com.jinxedstudios.hellwalker.item;
 
 import com.jinxedstudios.hellwalker.HellwalkerEngine;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item BLUE_KEYCARD = registerItem("blue_keycard", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(HellwalkerEngine.MOD_ID, name), item);
+    }
 
     public static void registerModItems(){
         HellwalkerEngine.LOGGER.info("Registering Mod Items for " + HellwalkerEngine.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(BLUE_KEYCARD);
+        });
     }
 }
